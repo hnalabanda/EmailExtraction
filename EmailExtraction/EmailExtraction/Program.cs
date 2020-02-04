8667f7b87974fa77b50bcb62ad443a43453be5ee
espe-on <eljoe0@googlemail.com>
2020-02-04 15:16:04 +0000

    general neatening up of the file

--- EmailExtraction/EmailExtraction/Program.cs
+++ EmailExtraction/EmailExtraction/Program.cs
@@ -11,54 +11,39 @@ namespace EmailExtraction
     {
         static void Main(string[] args)
         {
-            string textInput = File.ReadAllText("Data/sample.txt");
-         
-         //using array method
-        //  string[]inputArray = textInput.Split(" ");
-        // int counter = 0;
-        //
-        //  for (int i = 0; i < inputArray.Length; i++)
-        //  {
-        //      if (inputArray[i].Contains("@softwire.com"))
-        //      {
-        //          counter++;
-        //          //Console.WriteLine(inputArray[i]);
-        //      }
-        //  }
-        
-        //creating a dictionary
-        Dictionary<string,int> emailDictionary = new Dictionary<string,int>();
-        // emailDictionary.Add("softwire.com", 1);
-        
-        
-        //finding domains with regular expression
-        string pattern = @"\w@(\w*\.\w+){1,3}"; //this regex expression looks for words after a letter@ combo and allows for 1-3 dot word combinations 
-        Regex domainSearch=new Regex(pattern);
-        
-        //using domains for 
-        Match firstMatch = domainSearch.Matches(textInput).First();
-        //var domain = firstMatch.Groups[1];
+            //Importing our Data
+            string fileName = "sample.txt";
+            string filePath = "Data/" + fileName;
+            string textInput = File.ReadAllText(filePath);
 
-        foreach (Match result in domainSearch.Matches(textInput))
-        {
-            var domain = result.Groups[1].Value;
-            if (emailDictionary.ContainsKey(domain))
-            {
-                emailDictionary[domain]++;
-            }
-            else
-            {
-                emailDictionary.Add(domain,1);
-            }
-        }
+            //creating a dictionary
+            Dictionary<string, int> emailDictionary = new Dictionary<string, int>();
 
-        foreach (var entry in emailDictionary)
-        {
-            Console.WriteLine(entry.Key + " "  + entry.Value);
-        }
-        
-     
-      
+            //finding domains with regular expression
+            string
+                pattern =
+                    @"\w@(\w*\.\w+){1,3}";
+            Regex domainSearch = new Regex(pattern);
+
+            //storing domains and counts in dictionary
+            foreach (Match result in domainSearch.Matches(textInput))
+            {
+                var domain = result.Groups[1].Value;
+                if (emailDictionary.ContainsKey(domain))
+                {
+                    emailDictionary[domain]++;
+                }
+                else
+                {
+                    emailDictionary.Add(domain, 1);
+                }
+            }
+
+            // Prints the Dictionary with some padding
+            foreach (var entry in emailDictionary)
+            {
+                Console.WriteLine("The domain " + entry.Key + " appears " + entry.Value + " times.");
+            }
         }
     }
 }
