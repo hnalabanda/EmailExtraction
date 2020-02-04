17aadd4599eb37588b4f72c9cb4bcfb6b7725cd9
Espe-On <eljoe0@googlemail.com>
2020-02-04 11:46:22 +0000

    added .Contains string counter for @softwire emails

--- EmailExtraction/EmailExtraction/Program.cs
+++ EmailExtraction/EmailExtraction/Program.cs
@@ -9,7 +9,18 @@ namespace EmailExtraction
         {
          //   System.IO.StreamReader (Data/sample.txt);
          string textInput = File.ReadAllText("Data/sample.txt");
-         Console.Write(textInput);
+         string[]inputArray = textInput.Split(" ");
+         // Console.Write(inputArray[1]);
+         int counter = 0;
+         for (int i = 0; i < inputArray.Length; i++)
+         {
+             if (inputArray[i].Contains("@softwire.com"))
+             {
+                 counter++;
+                 //Console.WriteLine(inputArray[i]);
+             }
+         }
+         Console.WriteLine("The Number of Softwire emails is " +counter);
         }
     }
 }
