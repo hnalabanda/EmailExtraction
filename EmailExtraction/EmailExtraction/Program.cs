f59b232323ecd23b64d087da9126af7ef05a89cc
hnalabanda <hemanalabanda@gmail.com>
2020-02-04 12:17:45 +0000

    added reg expressions search

--- EmailExtraction/EmailExtraction/Program.cs
+++ EmailExtraction/EmailExtraction/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace EmailExtraction
 {
@@ -7,11 +8,14 @@ namespace EmailExtraction
     {
         static void Main(string[] args)
         {
+            
          //   System.IO.StreamReader (Data/sample.txt);
          string textInput = File.ReadAllText("Data/sample.txt");
-         string[]inputArray = textInput.Split(" ");
-         // Console.Write(inputArray[1]);
-         int counter = 0;
+         
+         //using array method
+      /*   string[]inputArray = textInput.Split(" ");
+        int counter = 0;
+        
          for (int i = 0; i < inputArray.Length; i++)
          {
              if (inputArray[i].Contains("@softwire.com"))
@@ -20,7 +24,12 @@ namespace EmailExtraction
                  //Console.WriteLine(inputArray[i]);
              }
          }
-         Console.WriteLine("The Number of Softwire emails is " +counter);
+        */
+         
+         //using reg expression
+         string pattern = "\\w(@softwire.com)";
+         Regex rx=new Regex(pattern);
+         Console.WriteLine("The Number of Softwire emails is " +rx.Matches(textInput).Count);
         }
     }
 }
